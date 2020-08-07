0ce1a4291cb757adc4b138d2c799d8363c4da7a6
barrie2709 <bart@depraatdoos.be>
2020-08-07 21:02:56 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -28,5 +28,37 @@ namespace GradeBook.GradeBooks
             return 'F';
             
         }
+
+        public override void CalculateStatistics()
+        {
+            int numberOfStudents = Students.Count;
+
+            if(numberOfStudents<5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order " +
+                    "to properly calculate a student's overall grade.");
+                return;
+
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            int numberOfStudents = Students.Count;
+
+            if(numberOfStudents<5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades" +
+                    "in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
